f8a4b47a9771aba10002c6dbcf701a4f135d96c8
weirme <weirme@weirmes-MacBook-Pro.local>
2021-10-31 09:17:16 +0800

    first commit

---
+++ app/src/main/java/com/appnext/ItemDetailFragment.java
@@ -0,0 +1,274 @@
+package com.appnext;
+
+import android.content.ClipData;
+import android.os.Bundle;
+import android.view.DragEvent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.appnext.placeholder.PlaceholderContent;
+import com.appnext.databinding.FragmentItemDetailBinding;
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.core.cartesian.series.Column;
+
+import com.anychart.charts.Scatter;
+import com.anychart.core.scatter.series.Marker;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.HoverMode;
+import com.anychart.enums.Position;
+import com.anychart.enums.TooltipPositionMode;
+import com.anychart.enums.MarkerType;
+import com.anychart.enums.TooltipDisplayMode;
+import com.anychart.graphics.vector.SolidFill;
+import com.anychart.graphics.vector.text.HAlign;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.anychart.core.cartesian.series.RangeColumn;
+
+/**
+ * A fragment representing a single Item detail screen.
+ * This fragment is either contained in a {@link ItemListFragment}
+ * in two-pane mode (on larger screen devices) or self-contained
+ * on handsets.
+ */
+public class ItemDetailFragment extends Fragment {
+
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The placeholder content this fragment is presenting.
+     */
+    private PlaceholderContent.PlaceholderItem mItem;
+    private CollapsingToolbarLayout mToolbarLayout;
+    private TextView mTextView;
+
+    private final View.OnDragListener dragListener = (v, event) -> {
+        if (event.getAction() == DragEvent.ACTION_DROP) {
+            ClipData.Item clipDataItem = event.getClipData().getItemAt(0);
+            mItem = PlaceholderContent.ITEM_MAP.get(clipDataItem.getText().toString());
+            updateContent();
+        }
+        return true;
+    };
+    private FragmentItemDetailBinding binding;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public ItemDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the placeholder content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = PlaceholderContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = FragmentItemDetailBinding.inflate(inflater, container, false);
+        View rootView = binding.getRoot();
+
+//        mToolbarLayout = rootView.findViewById(R.id.toolbar_layout);
+//        mTextView = binding.itemDetail;
+
+        // Show the placeholder content as text in a TextView & in the toolbar if available.
+//        updateContent();
+//        rootView.setOnDragListener(dragListener);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        AnyChartView columnChart = binding.columnView;
+        setupColumnView(columnChart);
+
+    }
+
+    private void setupColumnView(AnyChartView columnView) {
+        Cartesian cartesian = AnyChart.column();
+
+        List<DataEntry> data = new ArrayList<>();
+        for (int i = 0; i < 24; i++) {
+            data.add(new ValueDataEntry(String.format("%dh", i),2*i));
+        }
+
+        Column column = cartesian.column(data);
+
+//        column.tooltip()
+//                .titleFormat("{%X}")
+//                .position(Position.CENTER_BOTTOM)
+//                .anchor(Anchor.CENTER_BOTTOM)
+//                .offsetX(0d)
+//                .offsetY(5d)
+//                .format("${%Value}{groupsSeparator: }");
+
+        cartesian.animation(true);
+
+        cartesian.yScale().minimum(0d);
+        cartesian.yScale().maximum(60d);
+        cartesian.yAxis(0).labels().format("{%Value}m");
+
+//        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+//        cartesian.interactivity().hoverMode(HoverMode.BY_X);
+
+        columnView.setChart(cartesian);
+    }
+
+    private void setupRangeView(AnyChartView rangeView) {
+        Cartesian cartesian = AnyChart.cartesian();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new CustomDataEntry("Jan",6.1, 8.9));
+        data.add(new CustomDataEntry("Jan", 9.1, 10.4));
+        data.add(new CustomDataEntry("Feb", 4.6, 6.1));
+        data.add(new CustomDataEntry("Mar", 5.9, 8.1));
+        data.add(new CustomDataEntry("Apr", 7.8, 10.7));
+        data.add(new CustomDataEntry("May", 10.5, 13.7));
+        data.add(new CustomDataEntry("June", 13.8, 17));
+        data.add(new CustomDataEntry("July", 16.5, 18.5));
+        data.add(new CustomDataEntry("Aug", 17.8, 19));
+        data.add(new CustomDataEntry("Sep", 15.4, 17.8));
+        data.add(new CustomDataEntry("Oct", 12.7, 15.3));
+        data.add(new CustomDataEntry("Nov", 9.8, 13));
+        data.add(new CustomDataEntry("Dec", 9, 10.1));
+
+//        Set set = Set.instantiate();
+//        set.data(data);
+//        Mapping londonData = set.mapAs("{ x: 'x', high: 'londonHigh', low: 'londonLow' }");
+
+        RangeColumn columnLondon = cartesian.rangeColumn(data);
+        columnLondon.name("London");
+
+        cartesian.xAxis(true);
+        cartesian.yAxis(true);
+
+        cartesian.yScale()
+                .minimum(4d)
+                .maximum(20d);
+
+        cartesian.legend(true);
+
+        cartesian.yGrid(true)
+                .yMinorGrid(true);
+
+        cartesian.tooltip().titleFormat("{%SeriesName} ({%x})");
+
+        rangeView.setChart(cartesian);
+    }
+
+    private void setupScatterView(AnyChartView scatterView) {
+        Scatter scatter = AnyChart.scatter();
+
+//        scatter.animation(true);
+
+        scatter.xScale()
+                .minimum(0d)
+                .maximum(24d);
+//        scatter.xScale().tick
+        scatter.yScale()
+                .minimum(0d)
+                .maximum(60d);
+
+        scatter.yGrid(true);
+
+        scatter.yAxis(0).labels().format("{%Value}m");
+        scatter.xAxis(0).drawFirstLabel(false).labels().format("{%Value}h");
+
+
+        scatter.interactivity()
+                .hoverMode(HoverMode.BY_SPOT)
+                .spotRadius(0d);
+
+//        scatter.tooltip().displayMode(TooltipDisplayMode.UNION);
+
+        Marker marker = scatter.marker(getMarkerData());
+        marker.type(MarkerType.CIRCLE)
+                .size(2);
+
+        marker.selected()
+                .size(2);
+
+//        marker.hovered()
+//                .size(30d)
+//                .fill(new SolidFill("gold", 1d))
+//                .stroke("anychart.color.darken(gold)");
+
+//        marker.tooltip()
+//                .hAlign(HAlign.START)
+//                .format("Waiting time: ${%Value} min.\\nDuration: ${%X} min.");
+
+        scatterView.setChart(scatter);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    private void updateContent() {
+        if (mItem != null) {
+            mTextView.setText(mItem.details);
+            if (mToolbarLayout != null) {
+                mToolbarLayout.setTitle(mItem.content);
+            }
+        }
+    }
+
+    private class CustomDataEntry extends DataEntry {
+        public CustomDataEntry(String x, Number londonHigh, Number londonLow) {
+            setValue("x", x);
+            setValue("high", londonHigh);
+            setValue("low", londonLow);
+        }
+    }
+
+    private List<DataEntry> getMarkerData() {
+        List<DataEntry> data = new ArrayList<>();
+
+        data.add(new ValueDataEntry(4, 20));
+        data.add(new ValueDataEntry(3, 7));
+        data.add(new ValueDataEntry(4, 5));
+        data.add(new ValueDataEntry(4, 15));
+        data.add(new ValueDataEntry(3, 8));
+        data.add(new ValueDataEntry(4, 10));
+        data.add(new ValueDataEntry(2, 5));
+        data.add(new ValueDataEntry(4, 33));
+        data.add(new ValueDataEntry(1, 5));
+        data.add(new ValueDataEntry(4, 40));
+        data.add(new ValueDataEntry(1, 5));
+        data.add(new ValueDataEntry(4, 54));
+
+        return data;
+    }
+}
---
+++ app/src/main/java/com/appnext/ItemListFragment.java
@@ -0,0 +1,320 @@
+package com.appnext;
+
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.core.content.ContextCompat;
+import androidx.core.view.ViewCompat;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.ContentInfo;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.util.List;
+
+import com.appnext.databinding.FragmentItemListBinding;
+import com.appnext.databinding.ItemListContentBinding;
+
+import com.appnext.placeholder.PlaceholderContent;
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Polar;
+import com.anychart.data.Mapping;
+import com.anychart.data.Set;
+import com.anychart.enums.PolarSeriesType;
+import com.anychart.enums.ScaleStackMode;
+import com.anychart.enums.ScaleTypes;
+import com.anychart.enums.TooltipDisplayMode;
+import com.anychart.scales.Linear;
+
+import java.util.ArrayList;
+
+/**
+ * A fragment representing a list of Items. This fragment
+ * has different presentations for handset and larger screen devices. On
+ * handsets, the fragment presents a list of items, which when touched,
+ * lead to a {@link ItemDetailFragment} representing
+ * item details. On larger screens, the Navigation controller presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
+public class ItemListFragment extends Fragment implements View.OnClickListener {
+
+    /**
+     * Method to intercept global key events in the
+     * item list fragment to trigger keyboard shortcuts
+     * Currently provides a toast when Ctrl + Z and Ctrl + F
+     * are triggered
+     */
+    ViewCompat.OnUnhandledKeyEventListenerCompat unhandledKeyEventListenerCompat = (v, event) -> {
+        if (event.getKeyCode() == KeyEvent.KEYCODE_Z && event.isCtrlPressed()) {
+            Toast.makeText(
+                    v.getContext(),
+                    "Undo (Ctrl + Z) shortcut triggered",
+                    Toast.LENGTH_LONG
+            ).show();
+            return true;
+        } else if (event.getKeyCode() == KeyEvent.KEYCODE_F && event.isCtrlPressed()) {
+            Toast.makeText(
+                    v.getContext(),
+                    "Find (Ctrl + F) shortcut triggered",
+                    Toast.LENGTH_LONG
+            ).show();
+            return true;
+        }
+        return false;
+    };
+
+    private FragmentItemListBinding binding;
+    private MaterialButton weekly_button;
+    private MaterialButton daily_button;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentItemListBinding.inflate(inflater, container, false);
+        weekly_button = binding.weeklyButton;
+        weekly_button.setOnClickListener(this);
+        daily_button = binding.dailyButton;
+        daily_button.setOnClickListener(this);
+        daily_button.setSelected(true);
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ViewCompat.addOnUnhandledKeyEventListener(view, unhandledKeyEventListenerCompat);
+
+        AnyChartView pieView = binding.pieView;
+        setupPieView(pieView);
+
+        RecyclerView recyclerView = binding.itemList;
+
+        // Leaving this not using view binding as it relies on if the view is visible the current
+        // layout configuration (layout, layout-sw600dp)
+        View itemDetailFragmentContainer = view.findViewById(R.id.item_detail_nav_container);
+
+        /* Click Listener to trigger navigation based on if you have
+         * a single pane layout or two pane layout
+         */
+        View.OnClickListener onClickListener = itemView -> {
+            PlaceholderContent.PlaceholderItem item =
+                    (PlaceholderContent.PlaceholderItem) itemView.getTag();
+            Bundle arguments = new Bundle();
+            arguments.putString(ItemDetailFragment.ARG_ITEM_ID, item.id);
+            if (itemDetailFragmentContainer != null) {
+                Navigation.findNavController(itemDetailFragmentContainer)
+                        .navigate(R.id.fragment_item_detail, arguments);
+            } else {
+                Navigation.findNavController(itemView).navigate(R.id.show_item_detail, arguments);
+            }
+        };
+
+        /*
+         * Context click listener to handle Right click events
+         * from mice and trackpad input to provide a more native
+         * experience on larger screen devices
+         */
+        View.OnContextClickListener onContextClickListener = itemView -> {
+            PlaceholderContent.PlaceholderItem item =
+                    (PlaceholderContent.PlaceholderItem) itemView.getTag();
+            Toast.makeText(
+                    itemView.getContext(),
+                    "Context click of item " + item.id,
+                    Toast.LENGTH_LONG
+            ).show();
+            return true;
+        };
+
+        setupRecyclerView(recyclerView, onClickListener, onContextClickListener);
+    }
+
+    private void setupPieView(AnyChartView pieView) {
+        Polar polar = AnyChart.polar();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new CustomDataEntry("Nail polish", 12814, 4376, 4229));
+        data.add(new CustomDataEntry("Eyebrow pencil", 13012, 3987, 3932));
+        data.add(new CustomDataEntry("Rouge", 11624, 3574, 5221));
+        data.add(new CustomDataEntry("Pomade", 8814, 4376, 9256));
+        data.add(new CustomDataEntry("Eyeshadows", 12998, 4572, 3308));
+        data.add(new CustomDataEntry("Eyeliner", 12321, 3417, 5432));
+        data.add(new CustomDataEntry("Foundation", 10342, 5231, 13701));
+        data.add(new CustomDataEntry("Lip gloss", 22998, 4572, 4008));
+        data.add(new CustomDataEntry("Mascara", 11261, 6134, 18712));
+        data.add(new CustomDataEntry("Powder", 10261, 5134, 25712));
+
+        Set set = Set.instantiate();
+        set.data(data);
+        Mapping series1Data = set.mapAs("{ x: 'x', value: 'value' }");
+        Mapping series2Data = set.mapAs("{ x: 'x', value: 'value2' }");
+        Mapping series3Data = set.mapAs("{ x: 'x', value: 'value3' }");
+
+        polar.column(series1Data);
+
+        polar.column(series2Data);
+
+        polar.column(series3Data);
+
+//        polar.title("APP Usage Time");
+
+        polar.sortPointsByX(true)
+                .defaultSeriesType(PolarSeriesType.COLUMN)
+                .yAxis(false)
+                .xScale(ScaleTypes.ORDINAL);
+
+        polar.title().margin().bottom(20d);
+
+        ((Linear) polar.yScale(Linear.class)).stackMode(ScaleStackMode.VALUE);
+
+        polar.tooltip()
+                .valuePrefix("$")
+                .displayMode(TooltipDisplayMode.UNION);
+
+        pieView.setChart(polar);
+    }
+
+    private void setupRecyclerView(
+            RecyclerView recyclerView,
+            View.OnClickListener onClickListener,
+            View.OnContextClickListener onContextClickListener
+    ) {
+
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(
+                PlaceholderContent.ITEMS,
+                onClickListener,
+                onContextClickListener
+        ));
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.weekly_button:
+                weekly_button.setSelected(true);
+                daily_button.setSelected(false);
+                binding.dateText.setText("Daily");
+                binding.timeText.setText("3h 25m");
+                break;
+            case R.id.daily_button:
+                weekly_button.setSelected(false);
+                daily_button.setSelected(true);
+                binding.dateText.setText("Oct 1");
+                binding.timeText.setText("4h 30m");
+                break;
+        }
+    }
+
+    public static class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final List<PlaceholderContent.PlaceholderItem> mValues;
+        private final View.OnClickListener mOnClickListener;
+        private final View.OnContextClickListener mOnContextClickListener;
+
+        SimpleItemRecyclerViewAdapter(List<PlaceholderContent.PlaceholderItem> items,
+                                      View.OnClickListener onClickListener,
+                                      View.OnContextClickListener onContextClickListener) {
+            mValues = items;
+            mOnClickListener = onClickListener;
+            mOnContextClickListener = onContextClickListener;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            ItemListContentBinding binding =
+                    ItemListContentBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+            return new ViewHolder(binding);
+
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+//            holder.mIdView.setText(mValues.get(position).id);
+//            holder.mContentView.setText(mValues.get(position).content);
+
+            holder.itemView.setTag(mValues.get(position));
+            holder.itemView.setOnClickListener(mOnClickListener);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                holder.itemView.setOnContextClickListener(mOnContextClickListener);
+            }
+            holder.itemView.setOnLongClickListener(v -> {
+                // Setting the item id as the clip data so that the drop target is able to
+                // identify the id of the content
+                ClipData.Item clipItem = new ClipData.Item(mValues.get(position).id);
+                ClipData dragData = new ClipData(
+                        ((PlaceholderContent.PlaceholderItem) v.getTag()).content,
+                        new String[]{ClipDescription.MIMETYPE_TEXT_PLAIN},
+                        clipItem
+                );
+
+                if (Build.VERSION.SDK_INT >= 24) {
+                    v.startDragAndDrop(
+                            dragData,
+                            new View.DragShadowBuilder(v),
+                            null,
+                            0
+                    );
+                } else {
+                    v.startDrag(
+                            dragData,
+                            new View.DragShadowBuilder(v),
+                            null,
+                            0
+                    );
+                }
+                return true;
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+//            final TextView mIdView;
+//            final TextView mContentView;
+
+            ViewHolder(ItemListContentBinding binding) {
+                super(binding.getRoot());
+//                mIdView = binding.idText;
+//                mContentView = binding.content;
+            }
+
+        }
+    }
+
+    private class CustomDataEntry extends ValueDataEntry {
+        CustomDataEntry(String x, Number value, Number value2, Number value3) {
+            super(x, value);
+            setValue("value2", value2);
+            setValue("value3", value3);
+        }
+    }
+
+}
